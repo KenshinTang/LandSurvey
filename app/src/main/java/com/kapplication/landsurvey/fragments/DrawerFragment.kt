e873739facc2e131a19ffbbd1eb4f87b7c587247
KenshinTang <28135055@qq.com>
2018-10-12 15:35:58 +0800

    init ui

--- app/src/main/java/com/kapplication/landsurvey/fragments/DrawerFragment.kt
+++ app/src/main/java/com/kapplication/landsurvey/fragments/DrawerFragment.kt
@@ -3,14 +3,12 @@ package com.kapplication.landsurvey.fragments
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
-import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import com.kapplication.landsurvey.R
-import com.kapplication.landsurvey.adapter.DiscoverFragmentAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -32,10 +30,6 @@ class DrawerFragment : Fragment() {
     private var param2: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
-    private lateinit var mTabLayout: TabLayout
-    private lateinit var mViewPager : ViewPager
-    private val mTabTitles = arrayOf("Automatic mode", "Piling mode", "Manual mode")
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -47,17 +41,7 @@ class DrawerFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_drawer, container, false)
-        mTabLayout = view.findViewById(R.id.tablayout)
-        mViewPager = view.findViewById(R.id.viewpager)
-
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.mode_automatic))
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.mode_piling))
-        mTabLayout.addTab(mTabLayout.newTab().setText(R.string.mode_manual))
-
-        mViewPager.adapter = DiscoverFragmentAdapter(childFragmentManager, getString(R.string.mode_automatic), getString(R.string.mode_piling), getString(R.string.mode_manual))
-        mTabLayout.setupWithViewPager(mViewPager)
-        return view
+        return inflater.inflate(R.layout.fragment_drawer, container, false)
     }
 
     // TODO: Rename method, update argument and hook method into UI event
