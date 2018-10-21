216e219a3871e5b4b941b5c156c521b0d6c542a9
KenshinTang <28135055@qq.com>
2018-10-21 19:13:27 +0800

    add up action for detail view

--- app/src/main/java/com/kapplication/landsurvey/fragments/DetailDrawerFragment.kt
+++ app/src/main/java/com/kapplication/landsurvey/fragments/DetailDrawerFragment.kt
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import com.kapplication.landsurvey.R
 
@@ -40,8 +41,11 @@ class DetailDrawerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_drawer, container, false)
+        val view = inflater.inflate(R.layout.fragment_detail_drawer, container, false)
+        view.findViewById<TextView>(R.id.up).setOnClickListener {
+            fragmentManager?.popBackStack()
+        }
+        return view
     }
 
     // TODO: Rename method, update argument and hook method into UI event
